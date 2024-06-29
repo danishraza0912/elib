769fee0f5b87d7bc881391a92e1ba815e8d64e9a
Danish <danishraza24april@gmail.com>
2024-06-29 18:16:31 +0530

    usermodel

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import createHttpError from "http-errors"
+import userModel from "./userModel"
 
 
 const createdUser = async(req : Request, res: Response, next: NextFunction)=>{
@@ -9,11 +10,21 @@ const createdUser = async(req : Request, res: Response, next: NextFunction)=>{
         const error = createHttpError(400,"All fields are required")
         return next(error)
     }
-    res.send({
-        name,
-        email,
-        password
-    })
+    const user = await userModel.findOne({email})
+    if(user)
+    {
+            const error= createHttpError(400,"User already registered")
+            throw error
+    }
+    else{
+       // eslint-disable-next-line @typescript-eslint/no-unused-vars
+       const newuser= await userModel.create({
+            name,
+            email, 
+            password
+        })
+        res.send(newuser)
+    }
 }
 
 export default createdUser
