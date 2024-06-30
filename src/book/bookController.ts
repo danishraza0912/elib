48bca4de5d8d67e87da9b8d091869870fc01eb43
Danish <danishraza24april@gmail.com>
2024-06-30 16:59:14 +0530

    add files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,37 @@
-import { Request,Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import cloudinary from "../config/cloudinary"
+import path from "path"
+import createHttpError from "http-errors"
 
 const createdBook = async(req : Request, res: Response, next: NextFunction)=>{
-    res.json({message :"Hey from book"})
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] }
+    res.send(files)
+    
+    try{
+        const coverImgMimeType = files.coverImage[0].mimetype.split('/').at(-1)
+    const filename = files.coverImage[0].filename
+    const filepath =  path.join(__dirname, "../../public/data/uploads",filename) 
+    const coverimage=  cloudinary.uploader.upload(filepath, {
+        filename_override: filename,
+        folder: 'book-covers',
+        format: coverImgMimeType
+    })
+          const fileMimeType = files.file[0].mimetype.split('/').at(-1)
+    const pdffilename = files.file[0].filename
+    const pdffilepath =  path.join(__dirname, "../../public/data/uploads",pdffilename) 
+    const pdffile =  cloudinary.uploader.upload(pdffilepath, {
+        resource_type: 'raw',
+        filename_override: pdffilename,
+        folder: 'book-pdfs',
+        format: fileMimeType
+    })
+    console.log(pdffile, coverimage);
+    } catch(err)
+    {
+        console.log(err);
+        return next(createHttpError(404, "Files not uploaded on cloudinary"))
+    }
 }
 
 export default createdBook
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,10 @@ const _config= {
     port : process.env.PORT,
     databaseurl: process.env.databaseurl,
     NODE_ENV: process.env.NODE_ENV,
-    jwtSecret: process.env.jwtSecret
+    jwtSecret: process.env.jwtSecret,
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
 }
 
 export const config= Object.freeze(_config)
