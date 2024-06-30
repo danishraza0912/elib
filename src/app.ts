2fea6b3a92f16e1ac7f92f53ef76ce44597332a5
Danish <danishraza24april@gmail.com>
2024-06-30 12:42:27 +0530

    setup multer

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import createHttpError from "http-errors";
 import globalErrorHandler from "./middlewares/globalerrorhandlers";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import multer from "multer";
+import path from "path";
 
 const app = express()
 
@@ -14,7 +16,22 @@ app.get("/", (req, res)=>{
 
 app.use(express.json())
 app.use("/api/users", userRouter)
-app.use("/api/books", bookRouter)
+const upload= multer({
+    dest:path.join(__dirname, "../public/data/uploads"),
+    limits : {fileSize: 3e7}
+})
+
+app.use("/api/books", upload.fields([
+    {
+        name: "coverImage",
+        maxCount:1
+    },
+    {
+        name: "file",
+        maxCount:1
+    },
+
+]), bookRouter)
 
 app.use(globalErrorHandler)
 
